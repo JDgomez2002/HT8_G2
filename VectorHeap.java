c6c2b31e2649c803d15ca77ce22115254bce1566
JDgomez2002 <gom21429@uvg.edu.gt>
2022-04-07 08:18:30 -0600

    VectorHeap

--- VectorHeap.java
+++ VectorHeap.java
@@ -6,39 +6,52 @@
  * Daniel Gómez 21429
  * Grupo 2
  */
-import java.util.Vector;
+public class VectorHeap<E extends Comparable<E>> implements iPriorityQueue<E>{
 
-public class VectorHeap<E> {
-
-    protected Vector<E> data;  // the data, kept in heap order
-    public VectorHeap(){
-        data = new Vector<E>();
+    @Override
+    public E getFirst() {
+        // TODO Auto-generated method stub
+        return null;
     }
-    public VectorHeap(Vector<E> v)
-    // post: constructs a new priority queue from an unordered vector
-    {
-        int i;
-        data = new Vector<E>(v.size()); // we know ultimate size
-        for (i = 0; i < v.size(); i++)
-        {   // add elements to heap
-            data.add(v.get(i));
-} }
-    protected static int parent(int i)
-    // pre: 0 <= i < size
-    // post: returns parent of node at location i
-    {
-        return (i-1)/2;
+
+    @Override
+    public E remove() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void add(E value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void shiftDown(){
+
+    }
+
+    private void shiftUp(){
+
+    }
+    
 }
-    protected static int left(int i)
-    // pre: 0 <= i < size
-    // post: returns index of left child of node at location i
-    {
-        return 2*i+1;
-}
-    protected static int right(int i)
-    // pre: 0 <= i < size
-    // post: returns index of right child of node at location i
-    {
-        return 2*(i+1);
-}
-}
+
+
